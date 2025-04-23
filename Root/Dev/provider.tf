8911790279c800b0ae3c6fbc9dc0f3353040916e
manchal <manchalsharma08@gmail.com>
2025-04-24 00:24:41 +0530

    addd

--- Root/Dev/provider.tf
+++ Root/Dev/provider.tf
@@ -5,9 +5,16 @@ terraform {
       version = "4.26.0"
     }
   }
-
-
+backend "azurerm" {
+    resource_group_name  = "myman-rg "
+    storage_account_name = "mycvffdgfd434gffbvvbc444"
+    container_name       = "mycontainer"
+    key                 = "ram.terraform.tfstate"
+  }
+  
 }
+
+
 
 provider "azurerm" {
   features {}
